0ea6abc6b1e20e601c984c78bff26ed36fc9325b
Mateusz <mateuszrusinowicz@gmail.com>
2024-10-20 22:31:31 +0200

    review

--- src/test/java/com/circlek/Test01.java
+++ src/test/java/com/circlek/Test01.java
@@ -10,7 +10,7 @@ import static org.testng.Assert.assertEquals;
 
 public class Test01 {
 
-    //CODE REVIEW
+    //CODE REVIEW121212
     @Test
     public void assertTextOnPage(){
         System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
